9d9acfd2f618b3bdc07dc47f22e9fec6a68c5b2d
seoseunghyeon <tmdgustj@gmail.com>
2023-10-01 14:33:48 +0900

    feat: User domain에 불필요한 메서드 제거

--- src/main/java/com/zerobase/market/user/domain/User.java
+++ src/main/java/com/zerobase/market/user/domain/User.java
@@ -98,17 +98,6 @@ public class User implements UserDetails {
         this.username = username;
         this.password = password;
     }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return username;
-    }
-
     @Override
     public boolean isAccountNonExpired() {
         return false;
